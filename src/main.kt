e1cc00900b093bc56fc77cc24628c860ae94a8a3
gathangamaryann <maryanngathanga@gmail.com>
2022-02-26 19:59:49 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main(){
+    var school ="AkiraChix"
+    var char1 = (school[0])
+    var char2 = (school[1])
+    var char4 = (school[3])
+    var result = ("$char1$char2$char4")
+    println(result)
+    var X = "Maryann"
+    var Y = 21
+    //  var result =
+    println("Hi my name is $X and i am $Y years")
+    var name = "Maryann"
+    println(name.length)
+    var jina="Gathanga"
+    if (jina=="Gathanga"){
+        println("thats me")
+    }else{
+        println("i dont know who that is")
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
